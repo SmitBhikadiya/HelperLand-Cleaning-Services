c159895feb92fd3f2adbd340ddbbf8d49090641a
Smit Patel <sbhikadiya892@rku.ac.in>
2021-12-23 23:23:37 +0530

    Changed css, html related to navbar and footer

--- HTML/static/js/footer.js
+++ HTML/static/js/footer.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $(".okbtn").on("click",function(){
         $("#section-cookie").css("display","none");
-    });  
+    });
+    
+    $(window).scroll(function(){
+        if(this.scrollY > 450){
+            $(".scroll-icon").css("display","flex");
+            $("#section-cookie").css("z-index", "9999");
+            $(".message-icon").css("bottom","190px");
+        }else{
+            $(".scroll-icon").css("display","none");
+            $("#section-cookie").css("z-index", "-1");
+            $(".message-icon").css("bottom","70px");
+        }
+    });
 });
