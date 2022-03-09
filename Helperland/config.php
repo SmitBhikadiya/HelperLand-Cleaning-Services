37a052be48be7f499629e6773fa9f9c96a8e198b
Smit Patel <sbhikadiya892@rku.ac.in>
2022-03-10 02:11:10 +0530

    Update Config.php

--- Helperland/config.php
+++ Helperland/config.php
@@ -16,7 +16,7 @@ class Config{
     
     //Enter Email and Password to activate smtp service
     const SMTP_EMAIL = "sbhikadiya892@rku.ac.in";
-    const SMTP_PASS = "7648Smit7648";
+    const SMTP_PASS = "";
     
     /*-------------Contact US ------------*/
     const SUBJECT_TYPE = ['general', 'inquiry', 'renewal', 'revocation'];
